185a86a0cd929c06552938ed3a3c0931ee0a6a9c
David Petra Natanael <petragtr8@gmail.com>
2021-03-27 15:22:13 +0700

    update parameter

--- covsirphy/analysis/scenario.py
+++ covsirphy/analysis/scenario.py
@@ -1459,8 +1459,8 @@ class Scenario(Term):
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=seed)
         # Create pipeline for learning
         cv = linear_model.MultiTaskElasticNetCV(
-            alphas=[0.001, 0.005, 0.009, 0.01, 0.1, 1, 10, 50, 90, 100],
-            l1_ratio=[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
+            alphas=[0.01, 0.1, 1, 10, 50, 90, 100],
+            l1_ratio=[0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
             cv=5, n_jobs=-1)
         steps = [
             ("scaler", MinMaxScaler()),
